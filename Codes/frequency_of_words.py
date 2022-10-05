22c586e40cbb9a4e04436fd4cd7877f45e0eeccb
Priyanshuiiitg <98309398+Priyanshuiiitg@users.noreply.github.com>
2022-10-05 15:28:50 +0530

    string_having_maximum_frequency.cpp

--- Codes/frequency_of_words.py
+++ Codes/frequency_of_words.py
@@ -1,4 +1,30 @@
-from collections import Counter
-line_text = "As a part of text analytics, we frequently need to count words and assign weightage to them for processing in various algorithms, so in this article we will see how we can find the frequency of each word in a given sentence. We can do it with three approaches as shown below."
-freq = Counter(line_text.split()).most_common()
-[print(word, count) for word, count in freq]
+string maxFrequency(vector<string> str)
+{
+
+  map<string,int> m;
+  for(int i=0;i<str.size();i++)
+    {
+        m[str[i]]++;
+    }
+  int ma=INT_MIN;
+  string s="";
+  for(auto &val:m)
+  {
+    if(val.second>ma)
+    {
+
+      ma=val.second;
+      s=val.first;
+    }
+      
+    
+    
+    
+  }
+  
+  return s;
+  
+  
+  
+  
+}
